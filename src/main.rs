e1adc6d78d0ab1f31f0b376da6dbb70e6e3c704b
arist76 <surafelfikru76@gmail.com>
2024-08-04 20:50:38 +0300

    set up kernel and vga text writing

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+#![no_std]
+#![no_main]
+
+mod vga_buffer;
+
+use core::panic::PanicInfo;
+#[panic_handler]
+fn panic(_info : &PanicInfo) -> ! {
+    loop {}
+}
+
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    vga_buffer::print_something();
+
+    loop {}
+}
